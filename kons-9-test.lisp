13d05bbb54fdc9ba9d8ca7b9aeda3d570247d602
Luke Gorrie <luke@nuddy.net>
2022-09-30 17:35:06 +0200

    kons-9-test.lisp: Trivial comment change

--- kons-9-test.lisp
+++ kons-9-test.lisp
@@ -1,8 +1,10 @@
 ;; Simple tests for kons-9
 (require :asdf)
+
 (format t "~&Loading Kons-9..~%")
 (asdf:oos 'asdf:load-op :kons-9)
 (format t "~&Success loading Kons-9.~%")
+
 (format t "~&Testing Kons-9..~%")
 (asdf:oos 'asdf:test-op :kons-9)
 (format t "~&Success testing Kons-9~%")
